da3bd465d42b0289ce77781e1d26f0e1bfa03400
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-11-24 22:00:18 +0530

    Add files via upload

--- java_generics2.java
+++ java_generics2.java
@@ -1,5 +1,38 @@
-public class java_generics2 {
 
-    
-    
+class Pair<A, B> {
+    A first;
+    B second;
+
+    Pair(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    A getFirst() {
+        return first;
+    }
+
+    B getSecond() {
+        return second;
+    }
+
+    public static void main(String[] args) {
+        Pair<Integer, String> p1 ;
+        p1 = new Pair<Integer, String>(1, "apple"); // Generic Type
+        System.out.println(p1.getFirst());
+        System.out.println(p1.getSecond());
+
+        Pair<Integer, String> p2 ;
+        p2 = new Pair(2, "pear"); // Classic Type 
+        System.out.println(p2.getFirst());
+        System.out.println(p2.getSecond());
+
+
+        Pair<Integer, String> p3 ;
+        p3 = new Pair<>(3, "orange"); // Generic Type
+        System.out.println(p3.getFirst());
+        System.out.println(p3.getSecond());
+    }
+
 }
+
