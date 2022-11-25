2d85f5b51f77e3e760436df9e22dbd892dcbc226
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-11-25 23:52:06 +0530

    Add files via upload

--- java_generics8.java
+++ java_generics8.java
@@ -1,18 +1,16 @@
 public class java_generics8<T> {
     T[] data;
     java_generics8(int capacity) {
-        data = (T[]) new Object[capacity];
+        data = (T[]) new Integer[capacity];
     }
    public static void main(String[] args) {
         java_generics8<Integer> p1;
         p1 = new java_generics8<Integer>(2);
         p1.data[0] = 1;
         p1.data[1] = 2;
-        for (Integer i =0 ; i < p1.data.length; i++) {
+        for (Integer i = 0; i < p1.data.length; i++) {
             System.out.println(p1.data[i]);
         }
-         
     }
-    
     
 }
