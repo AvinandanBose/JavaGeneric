fb0d9076e3b938793bcd538bf17e115093f6e870
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-11-27 00:48:46 +0530

    Add files via upload

--- java_generics21.java
+++ java_generics21.java
@@ -3,11 +3,16 @@ public class java_generics21 {
         System.out.println("The value is: " + data);
     }
 
+    public static <T> Integer printElem2(T data) {
+       return (Integer)data;
+    }
+
     public static void main(String[] args) {
         printElem("Hello");
         printElem(10);
         printElem(10.5);
         printElem(true);
+        System.out.println("The value is: " + printElem2(10));
     }
 }
 
