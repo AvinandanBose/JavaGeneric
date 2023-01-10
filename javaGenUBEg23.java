4873181d376f9389297269a1b06f834d9e2098c7
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-01-10 10:31:07 +0530

    Add files via upload

--- javaGenUBEg23.java
+++ javaGenUBEg23.java
@@ -1,5 +1,5 @@
 interface Example1 {
-    public void addI(int a, int  b);
+    public void addI(int a, int b);
 }
 
 class InterfaceUBb implements Example1 {
@@ -7,21 +7,26 @@ class InterfaceUBb implements Example1 {
     public void addI(int a, int b) {
         System.out.println(a + " + " + b + "=" + (a + b));
     }
-    
+
 }
 
 public class javaGenUBEg23<T extends Example1> {
     T value;
+
     public javaGenUBEg23(T value) {
         this.value = value;
     }
+
     public T getValue() {
         return value;
     }
+
     public static void main(String[] args) {
         javaGenUBEg23<InterfaceUBb> obj = new javaGenUBEg23<>(new InterfaceUBb());
         obj.value.addI(10, 20);
+        InterfaceUBb obj1 = new InterfaceUBb();
+        obj1.addI(80, 40);
+        javaGenUBEg23<InterfaceUBb> obj2 = new javaGenUBEg23<>(obj1);
+        obj2.value.addI(50, 60);
     }
 }
-
-
