187212bc28c684887e968ce622ee1c145bf25f31
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-02-11 16:36:00 +0530

    Add files via upload

---
+++ UpperBoundWildCardMeth11.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class UpperBoundWildCardMeth11 {
+	public static Set<? extends Number> test(Set<? extends Number> num) {
+
+		System.out.println("Set elements: " + num);
+
+		for (Number n : num) {
+			System.out.println(n);
+		}
+
+		Iterator<? extends Number> it = num.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+
+		Iterable<? extends Number> it2 = num;
+		for (Number n : it2) {
+			System.out.println(n);
+		}
+		return num;
+	}
+	
+	public static void main(String[] args) {
+		Set<Integer> set = new HashSet<>();
+		
+		set.add(1);
+		set.add(2);
+		set.add(3);
+		set.add(4);
+		set.add(5);
+		set.add(6);
+		set.add(7);
+		set.add(10);
+		set.add(11);
+		set.add(12);
+		
+		System.out.println(test(set));
+	}
+}
