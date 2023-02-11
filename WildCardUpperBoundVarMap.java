2a543d38edd2eac3aded28e775439fb2a27b4b90
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-02-11 20:02:11 +0530

    Add files via upload

--- WildCardUpperBoundVarMap.java
+++ WildCardUpperBoundVarMap.java
@@ -1,15 +1,18 @@
 package com.company;
 
+import java.util.HashMap;
 import java.util.Map;
 
 
 public class WildCardUpperBoundVarMap {
 	public static void main(String[]args) {
-		Map<String,Integer> map = Map.of("A",1 , "B", 2 , "C", 3 );
+		Map<? extends String, ? extends Integer> map = Map.of("A",1 , "B", 2 , "C", 3 );
 		System.out.println(map);
 		Boolean c = map.containsKey("A");
 		System.out.println(c);
 		map.forEach((K,V)-> System.out.println("Values =" + V));
+		
+	
 
 	}
 }
