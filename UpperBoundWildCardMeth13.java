e27f2cbc24e95fe243f1e1e5d4d661b95e2741a4
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-02-11 17:47:00 +0530

    Add files via upload

---
+++ UpperBoundWildCardMeth13.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class UpperBoundWildCardMeth13 {
+	public static Map<? extends String,? extends Number> test(Map<? extends String,? extends Number> map) {
+		System.out.println("Set elements: " + map);
+
+		for (String key : map.keySet()) {
+			System.out.println("Keys ="+ key);
+		}
+		
+		for (Number num : map.values()) {
+			System.out.println(num);
+		}
+
+		Iterator<? extends String> it = map.keySet().iterator();
+		while (it.hasNext()) {
+			System.out.println("Keys=" + it.next());
+		}
+
+		Iterable<? extends Number> it2 = map.values();
+		for (Number n : it2) {
+			System.out.println("Values =" +n);
+		}
+		
+		for( Map.Entry<? extends String,? extends Number> entry : map.entrySet()){
+            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+        }
+
+		return map;
+		
+	}
+	
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<>();
+		map.put("One", 1);
+		map.put("Two", 2);
+		map.put("Three", 3);
+		map.put("Four", 4);
+		map.put("Five", 5);
+		test(map);
+	}
+	
+}
