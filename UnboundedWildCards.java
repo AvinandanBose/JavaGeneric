eeae6776fc1decf23ccbbd0cacb80898301560dc
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-02-14 18:35:36 +0530

    Add files via upload

---
+++ UnboundedWildCards.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnboundedWildCards<T extends List<?>> {
+
+    public void add(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public static void main(String[] args) {
+
+        UnboundedWildCards<ArrayList<Integer>> a = new UnboundedWildCards<>();
+        UnboundedWildCards<ArrayList<Float>> b = new UnboundedWildCards<>();
+        UnboundedWildCards<ArrayList<Double>> c = new UnboundedWildCards<>();
+
+        // .....etc.
+
+        a.add(1, 2);
+        b.add(1, 2);
+        c.add(1, 2);
+
+    }
+
+}
