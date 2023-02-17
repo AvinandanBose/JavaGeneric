e9c763d91d222f5ed0d049a673161366e626a0e5
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-02-17 12:43:14 +0530

    Add files via upload

--- GenHierarchy8.java
+++ GenHierarchy8.java
@@ -1,32 +1,33 @@
-class Gen<T> {
+class Gen9<T> {
     T ob;
 
-    Gen(T o) {
+    Gen9(T o) {
         ob = o;
     }
 
     T getob() {
+        System.out.print("Gen9's getob(): ");
         return ob;
     }
 }
 
-class Gen2<T> extends Gen<T> {
-    Gen2(T o) {
+class Gen10<T> extends Gen9<T> {
+    Gen10(T o) {
         super(o);
     }
 
     // Override getob()
     T getob() {
-        System.out.print("You called Gen2's getob(): ");
+        System.out.print("Gen10's getob(): ");
         return ob;
     }
 }
 
 class OverrideDemo {
     public static void main(String args[]) {
-        Gen<Integer> iOb = new Gen<Integer>(88);
-        Gen2<Integer> iOb2 = new Gen2<Integer>(99);
-        Gen2<String> strOb2 = new Gen2<String>("Generics Test");
+        Gen9<Integer> iOb = new Gen9<Integer>(88);
+        Gen10<Integer> iOb2 = new Gen10<Integer>(99);
+        Gen10<String> strOb2 = new Gen10<String>("Generics Test");
 
         System.out.println(iOb.getob());
         System.out.println(iOb2.getob());
