fa32be9a145c7d12c75a8990d9d8d04f6a61eccb
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-02-23 20:12:29 +0530

    Add files via upload

--- ConstructorRefGen8.java
+++ ConstructorRefGen8.java
@@ -1,7 +1,16 @@
 import java.util.List;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-interface ImplGen<T extends List<? super Number>> {
+interface ImplGen<T extends List<Object>> {
+   CImplGen<T> add(int a, int b);
+}
+
+interface ImplGen1<T extends List<Serializable>> {
+   CImplGen<T> add(int a, int b);
+}
+
+interface ImplGen2<T extends List<Number>> {
    CImplGen<T> add(int a, int b);
 }
 
@@ -13,8 +22,16 @@ class CImplGen<T extends List<? super Number>> {
 public class ConstructorRefGen8 {
 
       public static void main(String[] args) {
-         ImplGen<ArrayList<Number>> ref = CImplGen::new;
+         ImplGen<ArrayList<Object>> ref = CImplGen::new;
          ref.add(10, 20);
+
+         ImplGen1<ArrayList<Serializable>> ref1 = CImplGen::new;
+         ref1.add(10, 20);
+
+         ImplGen2<ArrayList<Number>> ref2 = CImplGen::new;
+         ref2.add(10, 20);
       }
+
+
     
 }
