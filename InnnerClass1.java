5fad8a900a984c0d47d4037dc080cdcbccbeed2d
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2023-03-26 17:34:36 +0530

    Add files via upload

--- InnnerClass1.java
+++ InnnerClass1.java
@@ -11,7 +11,7 @@ public class InnnerClass1<T> {
     public static void main(String[] args) {
 
         InnnerClass1<String> innnerClass1 = new InnnerClass1<String>();
-        InnnerClass1<String>.InnerClass<String> innerClass1 = innnerClass1.new InnerClass<String>();
+        InnnerClass1<String>.InnerClass<String> innerClass1 = innnerClass1.new InnerClass<>();
         System.out.println(innerClass1.printer("Hello World"));
        
         
